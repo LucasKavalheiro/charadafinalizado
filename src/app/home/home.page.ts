6dcc19bda286e0c1d8af8dfbebc42c16f3b14b14
LucasKavalheiro <lukazkav222@gmail.com>
2022-11-21 22:43:30 -0300

    Projeto ainda em desenvolvimento!

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { checkServerIdentity } from 'tls';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  pergunta: string = "";
+  resposta: string = "";
+  animacao: boolean = false;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(){
+    this.solicitarCharada();
+    this.exibirResposta();
+  }
+
+  exibirResposta(){
+  
+  }
+
+  solicitarCharada(){
+    this.animacao = false;
+    const url = "http://lucasreno.kinghost.net/charada";
+    this.http.get<any[]>(url).subscribe( response => {
+      this.pergunta = response[0].pergunta;
+      
+      this.resposta = response[0].resposta;
+      this.animacao = true;
+    });
+  }
+
+  
+}
